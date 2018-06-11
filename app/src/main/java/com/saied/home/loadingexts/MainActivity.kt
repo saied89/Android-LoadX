029c9496b3f76075b840203acc1d521a1830e85d
saied <saiedmomen@gmail.com>
2018-06-11 23:23:46 +0430

    working on linear layout

--- app/src/main/java/com/saied/home/loadingexts/MainActivity.kt
+++ app/src/main/java/com/saied/home/loadingexts/MainActivity.kt
@@ -13,8 +13,20 @@ class MainActivity : AppCompatActivity() {
         loadButtonRelative.setOnClickListener {
             it.loadingV2(true)
         }
+        relativeLayout.setOnClickListener {
+            loadButtonRelative.loadingV2(false)
+        }
         loadButtonLinear.setOnClickListener {
             it.loadingV2(true)
+        }
+        linearLayoutVertical.setOnClickListener {
+            loadButtonLinear.loadingV2(false)
+        }
+        loadButtonLinearHorizontal.setOnClickListener {
+            it.loadingV2(true)
+        }
+        linearLayoutHorizonal.setOnClickListener {
+            loadButtonLinearHorizontal.loadingV2(false)
         }
     }
 }
